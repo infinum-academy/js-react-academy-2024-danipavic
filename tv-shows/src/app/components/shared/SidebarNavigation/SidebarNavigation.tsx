487da6c34ca3692a4987aa5aac6f76be5deeafd5
Dani Pavic <dani.pavic@infinum.com>
2024-07-09 15:54:52 +0200

    Rename pages

--- tv-shows/src/app/components/shared/SidebarNavigation/SidebarNavigation.tsx
+++ tv-shows/src/app/components/shared/SidebarNavigation/SidebarNavigation.tsx
@@ -9,18 +9,10 @@ export const SidebarNavigation = () => {
 		<VStack maxH="100vh" as="aside" p="6" align="flex-start">
 			<VStack align="flex-start" mb="auto">
 				<Header />
-				<Button as={NextLink} href={'/shows'} fontSize="2xl" mb="4" fontWeight="light" color="white" variant="link">
+				<Button as={NextLink} href={'/all-shows'} fontSize="2xl" mb="4" fontWeight="light" color="white" variant="link">
 					All shows
 				</Button>
-				<Button
-					as={NextLink}
-					href={'/shows/top-rated'}
-					fontSize="2xl"
-					mb="4"
-					fontWeight="light"
-					color="white"
-					variant="link"
-				>
+				<Button as={NextLink} href={'/top-rated'} fontSize="2xl" mb="4" fontWeight="light" color="white" variant="link">
 					Top rated
 				</Button>
 				<Button as={NextLink} href={'/'} fontSize="2xl" mb="4" fontWeight="light" color="white" variant="link">
