bbf706d8a78664b136b4e35da7b3677b90ff860d
Dani Pavic <dani.pavic@infinum.com>
2024-07-09 13:35:06 +0200

    Add error state

--- tv-shows/src/app/components/features/review/ReviewForm/ReviewForm.tsx
+++ tv-shows/src/app/components/features/review/ReviewForm/ReviewForm.tsx
@@ -1,72 +1,64 @@
-"use client";
+'use client';
 
-import { Input, Textarea } from "@chakra-ui/react";
-import { useState } from "react";
-import { IReview } from "../../../../typings/Review.type";
-import { StyledButton } from "../../../core/Button/Button";
-import { StarsRating } from "../../../shared/StarsRating/StarsRating";
+import { Input, Text, Textarea } from '@chakra-ui/react';
+import { useState } from 'react';
+import { IReview } from '../../../../typings/Review.type';
+import { StyledButton } from '../../../core/Button/Button';
+import { StarsRating } from '../../../shared/StarsRating/StarsRating';
 
 interface IReviewFormProps {
-  onAddReview: (review: IReview) => void;
+	onAddReview: (review: IReview) => void;
 }
 
 export const ReviewForm = ({ onAddReview }: IReviewFormProps) => {
-  const [selectedRating, setSelectedRating] = useState<number>(0);
+	const [selectedRating, setSelectedRating] = useState<number>(0);
+	const [error, setError] = useState<string | null>(null);
 
-  return (
-    <>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
+	return (
+		<>
+			<form
+				onSubmit={(event) => {
+					event.preventDefault();
 
-          const target = event.target as HTMLFormElement;
+					const target = event.target as HTMLFormElement;
 
-          const commentInput = target.elements.namedItem(
-            "comment"
-          ) as HTMLInputElement;
-          const ratingInput = target.elements.namedItem(
-            "rating"
-          ) as HTMLInputElement;
+					const commentInput = target.elements.namedItem('comment') as HTMLInputElement;
+					const ratingInput = target.elements.namedItem('rating') as HTMLInputElement;
 
-          if (!parseInt(ratingInput.value)) return;
+					if (!parseInt(ratingInput.value) || !commentInput.value) {
+						setError('Please fill in all fields');
 
-          const newReview: IReview = {
-            comment: commentInput.value,
-            rating: parseInt(ratingInput.value),
-            uuid: window.crypto.randomUUID(),
-            reviewerAvatarURL: "https://i.pravatar.cc/150?img=68",
-            reviewerEmail: "dani.pavic@infinum.com",
-          };
+						return;
+					} else {
+						setError(null);
+					}
 
-          commentInput.value = "";
-          ratingInput.value = "";
+					const newReview: IReview = {
+						comment: commentInput.value,
+						rating: parseInt(ratingInput.value),
+						uuid: window.crypto.randomUUID(),
+						reviewerAvatarURL: 'https://i.pravatar.cc/150?img=68',
+						reviewerEmail: 'dani.pavic@infinum.com',
+					};
 
-          onAddReview(newReview);
+					commentInput.value = '';
+					ratingInput.value = '';
 
-          setSelectedRating(0);
-        }}
-      >
-        <Textarea
-          borderRadius="2xl"
-          backgroundColor="white"
-          placeholder="Add a review"
-          required
-          name="comment"
-        />
+					onAddReview(newReview);
 
-        <Input
-          value={selectedRating ?? 0}
-          readOnly
-          name="rating"
-          display="none"
-        />
-        <StarsRating
-          canInteract={true}
-          rating={selectedRating}
-          setSelectedRating={setSelectedRating}
-        />
-        <StyledButton type="submit" label="Post" />
-      </form>
-    </>
-  );
+					setSelectedRating(0);
+				}}
+			>
+				<Textarea borderRadius="2xl" backgroundColor="white" placeholder="Add a review" name="comment" />
+				<Input value={selectedRating ?? 0} readOnly name="rating" display="none" />
+				{error && (
+					<Text mt="4" color="red.500">
+						{error}
+					</Text>
+				)}
+				<StarsRating canInteract={true} rating={selectedRating} setSelectedRating={setSelectedRating} />
+				<StyledButton type="submit" label="Post" />
+			</form>
+		</>
+	);
 };
