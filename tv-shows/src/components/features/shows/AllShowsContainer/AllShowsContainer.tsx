883e0ab32092512e4ab606c7cd6f43bd0590392b
Dani Pavic <dani.pavic@infinum.com>
2024-07-16 17:04:45 +0200

    Update useSWR keys

--- tv-shows/src/components/features/shows/AllShowsContainer/AllShowsContainer.tsx
+++ tv-shows/src/components/features/shows/AllShowsContainer/AllShowsContainer.tsx
@@ -2,12 +2,13 @@
 
 import useSWR from 'swr';
 import { getManyShows } from '../../../../fetchers/show';
+import { swrKeys } from '../../../../fetchers/swrKeys';
 import { ErrorMessage } from '../../../shared/ErrorMessage/ErrorMessage';
 import { Loader } from '../../../shared/Loader/Loader';
 import { ShowsList } from '../ShowsList/ShowsList';
 
 export function AllShowsContainer() {
-	const { data, error, isLoading } = useSWR('/api/shows', getManyShows);
+	const { data, error, isLoading } = useSWR(swrKeys.shows, getManyShows);
 
 	if (error) {
 		return <ErrorMessage />;
