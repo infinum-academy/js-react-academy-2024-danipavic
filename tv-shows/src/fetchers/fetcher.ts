e3c957f78eb9e148b8b2c017b21dc99afb4c9210
Dani Pavic <dani.pavic@infinum.com>
2024-07-16 16:57:41 +0200

    Add headers fallback for fetcher

--- tv-shows/src/fetchers/fetcher.ts
+++ tv-shows/src/fetchers/fetcher.ts
@@ -2,7 +2,9 @@ import { AUTH_LOCAL_STORAGE_KEY } from '../constants';
 import { ILocalStorageAuth, loadFromLocalStorage } from '../utils/localstorage-helpers';
 
 export async function fetcher<T>(input: string | URL | globalThis.Request, init?: RequestInit): Promise<T> {
-	const headers = new Headers(loadFromLocalStorage<ILocalStorageAuth>(AUTH_LOCAL_STORAGE_KEY) as unknown as Headers);
+	const headers = new Headers(
+		(loadFromLocalStorage<ILocalStorageAuth>(AUTH_LOCAL_STORAGE_KEY) ?? {}) as unknown as Headers
+	);
 
 	try {
 		const response = await fetch(input, { ...init, headers });
